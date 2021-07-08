a86bcb5b55d281346e9e59b909e7b2af56b18e51
boedhack <rudipratama070@gmail.com>
2021-07-08 23:02:53 +0700

    lineage

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 Extended Project
+# Copyright (C) 2021 LineageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Extended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
