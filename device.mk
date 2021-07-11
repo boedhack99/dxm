3ff8947a4b2deae9256e8dfb4607f6f027f687d2
boedhack <rudipratama070@gmail.com>
2021-07-11 16:22:13 +0700

    update

--- device.mk
+++ device.mk
@@ -401,8 +401,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-     android.hardware.power-service.mojito-libperfmgr \
-     android.hardware.power.stats@1.0-service.mojito
+    android.hardware.power-service.mojito-libperfmgr \
+    android.hardware.power.stats@1.0-service.mojito
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
